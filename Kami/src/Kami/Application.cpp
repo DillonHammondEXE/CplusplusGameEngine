6ae93e489ed2daa30ace6a4796b3440e0d53ab9c
KamiSlayer <lightningblaze777@yahoo.com>
2024-01-31 18:35:20 -0800

    Fixed Linker bugs, working window

--- Kami/src/Kami/Application.cpp
+++ Kami/src/Kami/Application.cpp
@@ -4,6 +4,8 @@
 #include "Kami/Events/ApplicationEvent.h"
 #include "Kami/Log.h"
 
+#include <GLFW/glfw3.h>
+
 namespace Kami {
 
 	Application::Application() 
@@ -18,6 +20,8 @@ namespace Kami {
 	{
 		while (m_Running)
 		{
+			glClearColor(1, 0, 1, 1);
+			glClear(GL_COLOR_BUFFER_BIT);
 			m_Window->OnUpdate();
 		}
 	}
